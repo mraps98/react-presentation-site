87825a7f11fa3d4f95b357bb1afb0f1bf942efe2
Singh <asingh2s@semo.edu>
2020-10-21 18:28:52 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import Contact from './components/contact';
+import "./css/index.css";
+import * as Info from './info';
+import MainPhoto from './images/mainphoto.jpg';
+
+function App() {
+  return (
+    <div class="index-content">
+
+      <div class="short-biography">
+        <img id = "user-image" src= {MainPhoto} />
+        <h1>I'm {Info.FullName}</h1>
+        <h2>A {Info.JobTitle} at <a href={Info.CompanyWebsite}>{Info.CompanyName}</a></h2>
+      </div>
+
+      <Contact />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/info.js
@@ -0,0 +1,18 @@
+export const FullName = 'Preet' //Copy and paste your name between the quotes
+
+export const GithubLink = 'https://github.com/mraps98' // your Github link within the quotes
+
+export const LinkedinLink = 'https://linkedin.com/in/amritpreet98' //Copy and paste your Linkedin link within the quotes
+
+export const Email = 'mailto:amritpreet98@gmail.com' //Replace # with email
+
+export const JobTitle = 'Student' //Copy and paste your job title within the quotes
+
+export const CompanyName = 'SEMO' //Copy and paste your company/school name within the quotes
+
+export const CompanyWebsite = 'https://semo.edu/' //Copy and paste your company/school name within the quotes
+
+//Replace /src/images/mainphoto.png with your own photo, and name it mainphoto.png
+
+//https:// must come before your links
+
